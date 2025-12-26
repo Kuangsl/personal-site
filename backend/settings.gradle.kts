187a7aa493c0ea5abf2d191f05f8562375aa1680
Kuangsl <690134117@qq.com>
2025-12-26 18:55:07 +0800

    一般修改

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,18 +1,18 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        mavenCentral()
+        gradlePluginPortal()  // 官方快
+        mavenCentral()        // 备
+        maven { name = "aliyun-plugin"; url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        mavenCentral()  // 优先官方
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        mavenCentral()  // 1st: 稳定快
+        maven { name = "aliyun-public"; url = uri("https://maven.aliyun.com/repository/public") }
         google()
+        maven { name = "aliyun-google"; url = uri("https://maven.aliyun.com/repository/google") }
     }
 }
 
